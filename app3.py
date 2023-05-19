ce84a47c3142d5df6789786f7e08126ba58425ad
Vitalijus Klicius <vitalijusklicius@gmail.com>
2023-05-19 16:20:17 +0300

    debuging

--- app3.py
+++ app3.py
@@ -1,57 +1,47 @@
-
 import streamlit as st
-from docx2txt import process as docx_process
 import io
 import base64
 import openai
 import time
-import tiktoken
 from docx import Document
+import tiktoken
 
-# Function to extract and preprocess text from document
-# Function to download result as txt file
+# Function to download the result as a txt file
 def get_text_download_link(text):
     b64 = base64.b64encode(text.encode()).decode()
     return f'<a href="data:file/txt;base64,{b64}" download="output.txt">Download Results as .txt</a>'
 
-    
-def text_preprocessing(file, split_lenght: int):
-    
-    def split_text_into_chunks(doc: str, lenght: int):
+# Function to split text into chunks
+def split_text_into_chunks(text, max_length):
+    tokenizer = tiktoken.get_encoding("cl100k_base")
+    tokens = tokenizer.encode(text) 
+    chunks = []
+    current_chunk = []
+    current_length = 0
+    for token in tokens:
+        if current_length + len(token) <= max_length:
+            current_chunk.append(token)
+            current_length += len(token)
+        else:
+            chunks.append(" ".join(current_chunk))
+            current_chunk = [token]
+            current_length = len(token)
+    if current_chunk:
+        chunks.append(" ".join(current_chunk))
+    return chunks
 
-        def decode_chunks(text):
-            return enc.decode(text)
+# Function to extract and preprocess text from document
+def preprocess_text(file, max_length):
+    document = Document(io.BytesIO(file.read()))
+    text = ' '.join([p.text for p in document.paragraphs])
+    return split_text_into_chunks(text, max_length)
 
-        def divide_chunks(l, n):
-            # looping till length l
-            for i in range(0, len(l), n):
-                yield l[i:i + n]   
-
-        enc = tiktoken.get_encoding("cl100k_base")
-
-        data_tokenized = enc.encode(doc) 
-        # How many elements each
-        # list should have
-        n = lenght
-
-        x = list(divide_chunks(data_tokenized, n))
-
-        output_list = list(map(decode_chunks, x))
-
-        return output_list
-
-    doc_doc = Document(io.BytesIO(file.read()))
-    doc = ' '.join([p.text for p in doc_doc.paragraphs])
-
-    result = split_text_into_chunks(doc, split_lenght)
-    
-    return result
-
-def make_request(question_input: str):
+# Function to make OpenAI API request
+def make_request(text):
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "system", "content": f"{question_input}"},
+            {"role": "system", "content": text},
         ]
     )
     return response
@@ -59,43 +49,34 @@ def make_request(question_input: str):
 # Streamlit App
 def main():
     st.title("Text Processing with ChatGPT")
-    
+
     api_key = st.text_input("Enter your OpenAI API key", type="password")
     chunk_size = st.slider("Select chunk size in tokens", 100, 3900, 1000)
-    
     command = st.text_area("Enter some text","Summarize the following text, as it would be yours and you would be the author, please  avoid such text as 'in this text' or 'author discusses':  ")
     uploaded_file = st.file_uploader("Upload a file (.docx)")
 
     if st.button('Process'):
-        if api_key == '':
+        if not api_key:
             st.write("Please provide your OpenAI API key.")
-
-        else:
-            openai.api_key = api_key
+            return
+        if not uploaded_file:
+            st.write("Please provide a document to process.")
+            return
         
-        if uploaded_file is not None:
-            chunks = text_preprocessing(uploaded_file, chunk_size)
-    
-        else:
-            st.write("Please provide some input (text and file).")
+        openai.api_key = api_key
+        chunks = preprocess_text(uploaded_file, chunk_size)
+        summarized = []
         
-        sumarized = []
-        for val in chunks:
-            
-            text = command + val
-            
-            st.write("Number of words in chunk: ~",len(val.split()))
-            
-            output = make_request(text)
-            response_text = output["choices"][0]["message"]["content"]
-            
-            st.write("Number of words in summarized text: ~",len(response_text.split()))
-
+        for chunk in chunks:
+            st.write("Number of words in chunk: ~", len(chunk.split()))
+            text = command + chunk
+            response = make_request(text)
+            summarized_chunk = response["choices"][0]["message"]["content"]
+            st.write("Number of words in summarized text: ~", len(summarized_chunk.split()))
             time.sleep(20)
-            
-            sumarized.append(output["choices"][0]["message"]["content"])
+            summarized.append(summarized_chunk)
         
-        results_txt = "\n\n".join(sumarized)
+        results_txt = "\n\n".join(summarized)
         st.markdown(get_text_download_link(results_txt), unsafe_allow_html=True)
 
 if __name__ == "__main__":
